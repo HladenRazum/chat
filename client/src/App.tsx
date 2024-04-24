4e1b539688e963ca15f965d08cc8356c85c60241
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2024-04-24 21:43:28 +0300

    fixed problem with imports aliases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
+import { Button } from '@/components/ui/Button';
+
 function App() {
   return (
-    <>
+    <div className='p-5'>
       <h1>Chat</h1>
-      <p className="text-red-500">
+      <p className='text-red-500'>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga qui
         molestias blanditiis, laboriosam atque veniam praesentium assumenda
         animi quidem velit.
       </p>
-    </>
+      <Button>Submit</Button>
+    </div>
   );
 }
 

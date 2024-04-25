ecd29964dd5a08f16677cc4311a49bff69fa7ae4
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2024-04-25 11:12:02 +0300

    added Separator component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import { Button } from '@/components/ui/Button';
+import { Separator } from '@/components/ui/Separator';
 
 function App() {
   return (
-    <div className='p-5'>
-      <h1>Chat</h1>
-      <p className='text-red-500'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga qui
-        molestias blanditiis, laboriosam atque veniam praesentium assumenda
-        animi quidem velit.
-      </p>
-      <Button>Submit</Button>
+    <div className='container'>
+      <div className='flex h-screen space-x-4 text-sm'>
+        <div className='w-[400px] p-5'>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            veritatis deleniti iusto et laboriosam. Dignissimos natus est eum
+            doloribus suscipit!
+          </p>
+        </div>
+        <Separator orientation='vertical' />
+        <div className='w-full p-5'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam qui
+            fugit quidem facilis animi earum sint culpa quasi assumenda
+            dignissimos perspiciatis adipisci reiciendis, eligendi quam quod
+            doloribus, omnis minus esse soluta officia ab sequi? Nulla,
+            incidunt. Facilis modi asperiores animi hic. Id harum recusandae
+            fugit consequatur ducimus facere dignissimos commodi?
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
